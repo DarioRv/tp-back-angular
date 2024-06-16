b684c75ea56ddae547688565a6a365d1c6cfde26
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-06-16 19:20:15 -0300

    add: servicios esenciales

---
+++ src/app/services/transaccion.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map, Observable } from 'rxjs';
+import { Transaccion } from '../interfaces/transaccion.interface';
+import { BackendResponse } from '../interfaces/backend-response.interface';
+
+export class TransaccionService {
+  private readonly baseUrl = environment.BACKEND_URL + 'transacciones';
+
+  constructor(private http: HttpClient) {}
+
+  getTransacciones(params?: any): Observable<Transaccion[]> {
+    const url = this.baseUrl;
+    return this.http
+      .get<BackendResponse<Transaccion[]>>(url, { params })
+      .pipe(map((res) => res.data));
+  }
+
+  create(transaccion: Transaccion): Observable<Transaccion> {
+    const url = this.baseUrl;
+    return this.http
+      .post<BackendResponse<Transaccion>>(url, transaccion)
+      .pipe(map((res) => res.data));
+  }
+}
